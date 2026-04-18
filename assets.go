2b249bd7da380367144a2b753e86d17dba2cd4c1
b0nehenge <metallica99mm@gamil.com>
2026-04-18 08:50:43 -0700

    added vieo upload to s3

--- assets.go
+++ assets.go
@@ -9,7 +9,7 @@ import (
 	"strings"
 )
 
-func (cfg *apiConfig) ensureAssetsDir() error {
+func (cfg apiConfig) ensureAssetsDir() error {
 	if _, err := os.Stat(cfg.assetsRoot); os.IsNotExist(err) {
 		return os.Mkdir(cfg.assetsRoot, 0755)
 	}
@@ -28,11 +28,15 @@ func getAssetPath(mediaType string) string {
 	return fmt.Sprintf("%s%s", id, ext)
 }
 
-func (cfg *apiConfig) getAssetDiskPath(assetPath string) string {
+func (cfg apiConfig) getObjectURL(key string) string {
+	return fmt.Sprintf("https://%s.s3.%s.amazonaws.com/%s", cfg.s3Bucket, cfg.s3Region, key)
+}
+
+func (cfg apiConfig) getAssetDiskPath(assetPath string) string {
 	return filepath.Join(cfg.assetsRoot, assetPath)
 }
 
-func (cfg *apiConfig) getAssetURL(assetPath string) string {
+func (cfg apiConfig) getAssetURL(assetPath string) string {
 	return fmt.Sprintf("http://localhost:%s/assets/%s", cfg.port, assetPath)
 }
 
